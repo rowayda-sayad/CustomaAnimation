402e8c5f022836ccd8de65c8e8d4acc75b84054d
Rowayda <rowaidasayad@gmail.com>
2021-08-11 01:20:50 +0300

    first commit

---
+++ app/src/main/java/com/example/customanimationudemy/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.customanimationudemy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.shake_animation);
+
+        imageView.startAnimation(animation);
+    }
+}
